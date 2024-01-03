bb334e530ff475d298c01010fc1c3b2acc4a40ac
Root Meet <rootmeet@gmail.com>
2024-01-03 16:07:49 -0700

    Jenkins-Docker-SonarQube-Terraform VM

---
+++ main.tf
@@ -0,0 +1,42 @@
+resource "aws_instance" "web" {
+    ami = "ami-0ce2cb35386fc22e9"
+    instance_type = "t2.large"
+    key_name = "my-ssh-key"
+    vpc_security_group_ids = [aws_security_group.Jenkins-VM-SG.id]
+    user_data = templatefile("./install.sh",{})
+
+    tags = {
+      name = "Jenkins-SonarQube"
+    }
+    root_block_device {
+      volume_size = 40
+    }
+}
+resource "aws_security_group" "Jenkins-VM-SG" {
+    name = "Jenkins-VM-SG"
+    description = "Allow TLS inbound traffic"
+
+    ingress = [
+        for port in[ 22, 80, 443, 8080, 9000, 3000 ] : {
+            description     = "inbound rules"
+            from_port       = port
+            to_port         = port
+            protocol        = "tcp"
+            cidr_blocks     = ["0.0.0.0/0"]
+            ipv6_cidr_block = []
+            prefix_list_ids = []
+            security_group  = []
+            self            = false
+        }
+    ]
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = [ "0.0.0.0/0" ]
+    }
+    tags = {
+      Name = "Jenkins-VM-SG"
+    }
+  
+}
