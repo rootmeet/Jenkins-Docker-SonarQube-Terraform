4c3e0167a5e76053fcb850545ef42acf120f002b
Root Meet <rootmeet@gmail.com>
2024-01-03 16:38:11 -0700

    reformatted

--- main.tf
+++ main.tf
@@ -18,15 +18,15 @@ resource "aws_security_group" "Jenkins-VM-SG" {
 
     ingress = [
         for port in[ 22, 80, 443, 8080, 9000, 3000 ] : {
-            description     = "inbound rules"
-            from_port       = port
-            to_port         = port
-            protocol        = "tcp"
-            cidr_blocks     = ["0.0.0.0/0"]
-          ipv6_cidr_blocks  = []
-          prefix_list_ids   = []
-          security_groups   = []
-            self            = false
+            description       = "inbound rules"
+            from_port         = port
+            to_port           = port
+            protocol          = "tcp"
+            cidr_blocks       = ["0.0.0.0/0"]
+            ipv6_cidr_blocks  = []
+            prefix_list_ids   = []
+            security_groups   = []
+            self              = false
         }
     ]
     egress {
